d3de415211732ecbe0410ad4757d9b4af5aeeb8c
cacadordealdeao-a11y <cacadordealdeao@gmail.com>
2026-03-09 18:38:13 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:just_audio/just_audio.dart';
-import 'package:on_audio_query/on_audio_query.dart';
 
-void main() => runApp(const MaterialApp(home: SoundBitHome()));
-
-class SoundBitHome extends StatelessWidget {
-  const SoundBitHome({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xFFF5F5DC),
-      appBar: AppBar(title: const Text("SOUND BIT"), backgroundColor: Colors.transparent, elevation: 0),
-      body: const Center(child: Text("O seu App de Música Retro", style: TextStyle(color: Color(0xFF808080)))),
-    );
-  }
-}
+void main() => runApp(const MaterialApp(home: Scaffold(
+  backgroundColor: Color(0xFFF5F5DC),
+  body: Center(child: Text("SOUND BIT PRONTO!", style: TextStyle(fontSize: 20, color: Color(0xFF808080)))),
+)));
