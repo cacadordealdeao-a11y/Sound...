6fd1c60b3a0858756c1731a8c8af7bfcb31430cc
cacadordealdeao-a11y <cacadordealdeao@gmail.com>
2026-03-09 18:43:54 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MaterialApp(home: Scaffold(
-  backgroundColor: Color(0xFFF5F5DC),
-  body: Center(child: Text("SOUND BIT PRONTO!", style: TextStyle(fontSize: 20, color: Color(0xFF808080)))),
-)));
+void main() => runApp(const MaterialApp(
+  home: Scaffold(
+    backgroundColor: Color(0xFFF5F5DC),
+    body: Center(
+      child: Text(
+        "SOUND BIT\nPRONTO PARA TOCAR!", 
+        textAlign: TextAlign.center,
+        style: TextStyle(fontSize: 20, color: Color(0xFF808080), fontWeight: FontWeight.bold)
+      ),
+    ),
+  ),
+));
